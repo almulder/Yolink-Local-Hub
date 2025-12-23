2b110c2d50b382832e5ebae8e235431b70b1f302
almulder <albertjmulder@gmail.com>
2025-12-22 21:36:00 -0700

    Update YoLink DoorSensor Device Local.groovy

--- Drivers/YoLink DoorSensor Device Local.groovy
+++ Drivers/YoLink DoorSensor Device Local.groovy
@@ -2,12 +2,13 @@
  *  YoLink™ Door Sensor (Local API Edition)
  *  © 2025 Albert Mulder
  *
+ *  1.1.2 - Updated for HPM
  *  1.1.1 - Harden the beginning of processStateData(String payload) to coerce/guard data and loraInfo before dereferencing.
  *  1.1.0 - Initial working driver
  */
 import groovy.json.JsonSlurper
 
-def clientVersion() { "1.1.1-DS-local" }
+def clientVersion() { "1.1.2" }
 def copyright()     { "© 2025 Albert Mulder" }
 def driverName()    { "YoLink™ Door Sensor (Local API Edition)" }
 
